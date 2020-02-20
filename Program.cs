17e8a2b830c2b8d525649adb09a08638efec2c23
Bolilyi Oleksandr <bolilyi.o@ukr.net>
2020-02-20 22:52:34 +0100

    Implement the generic method for the entity creation.

--- Program.cs
+++ Program.cs
@@ -6,6 +6,12 @@ namespace Musketeers
     class MainClass
     {
         #region Variables
+        private string _city = string.Empty;
+        private int _postalCode = 0;
+        private string _country = string.Empty;
+
+        private User _user = null;
+        private CarWorkshop _carWorkshop = null;
         private Dictionary<string, User> userDictionary = null;
         private Dictionary<string, CarWorkshop> carWorkshops = null;
         #endregion
@@ -27,13 +33,16 @@ namespace Musketeers
                 switch (input)
                 {
                     case 1:
-                        main.CreateUser();
+                        main.FillDictionary<User>(new User());
                         break;
                     case 2:
                         main.ShowUsers();
                         break;
                     case 3:
                         main.DeleteUserByUserName();
+                        break;
+                    case 4:
+                        main.FillDictionary<CarWorkshop>(new CarWorkshop());
                         break;
                     default:
                         Console.Write("Please select an option from the list.\n");
@@ -61,11 +70,48 @@ namespace Musketeers
             return input;
         }
 
-        private Dictionary<string, User> CreateUser()
+        private Dictionary<string, User> DeleteUserByUserName()
         {
-            var user = new User();
-            FillUserInformation(user);
+            Console.WriteLine("***********************************************");
+            Console.Write("Username: ");
+            var userName = Console.ReadLine();
 
+            if (userDictionary.ContainsKey(userName))
+                userDictionary.Remove(userName);
+            else
+                Console.WriteLine("User does not exist in the dictionary.");
+
+            return userDictionary;
+        }
+
+        private void ShowUsers()
+        {
+            if (userDictionary.Count == 0)
+                Console.WriteLine("There are no users in the dictionary.\n");
+            else
+            {
+                Console.WriteLine("-----------------------------------------------");
+                foreach (KeyValuePair<string, User> item in userDictionary)
+                {
+                    Console.WriteLine($"Username: {item.Value.Username}");
+                    Console.WriteLine($"Email: {item.Value.Email}");
+                    Console.WriteLine($"City: {item.Value.City}");
+                    Console.WriteLine($"Postal Code: {item.Value.PostalCode}");
+                    Console.WriteLine($"Country: {item.Value.Country}");
+                    Console.WriteLine();
+                }
+                Console.WriteLine("-----------------------------------------------\n");
+            }
+        }
+
+        private Dictionary<string, T> Create<T>(T entity)
+        {
+            if (entity is User)
+            {
+
+            }
+
+            /*
             if (string.IsNullOrEmpty(user.Username) || string.IsNullOrWhiteSpace(user.Username) ||
                 string.IsNullOrEmpty(user.Email) || string.IsNullOrWhiteSpace(user.Email))
             {
@@ -97,62 +143,104 @@ namespace Musketeers
             }
 
             return userDictionary;
+            */
+
+            return new Dictionary<string, T>();
         }
 
-        private Dictionary<string, User> DeleteUserByUserName()
+        private void FillDictionary<T>(T entity)
+        {
+            var dictionary = new Dictionary<string, T>();
+
+            if (entity is User)
+            {
+                _user = entity as User;
+                Create<T>((T)(object)FillInformation<User>(_user));
+            }
+
+            //if (entity is CarWorkshop)
+            //{
+            //    _carWorkshop = entity as CarWorkshop;
+            //    var result = FillInformation<CarWorkshop>(_carWorkshop);
+            //    dictionary.Add(_carWorkshop.CompanyName, (T)(object)result);
+            //}
+        }
+
+        private List<T> FillInformation<T>(T entity)
+        {
+            var list = new List<T>();
+
+            if (entity is User)
+            {
+                FillUserInformation(list);
+            }
+
+            if (entity is CarWorkshop)
+            {
+                FillCarWorkshopInformation(list);
+            }
+
+            return list;
+        }
+
+        private void FillCarWorkshopInformation<T>(List<T> list)
+        {
+            Console.WriteLine("***********************************************");
+            Console.Write("Company Name: ");
+            _carWorkshop.CompanyName = Console.ReadLine();
+
+            Console.Write("Car Trademarks: ");
+            _carWorkshop.CarTrademarks = Console.ReadLine();
+
+            var location = FillLocation();
+
+            _carWorkshop.City = location[0];
+            _carWorkshop.PostalCode = Convert.ToInt32(location[1]);
+            _carWorkshop.Country = location[2];
+
+            list.Add((T)(object)_carWorkshop);
+        }
+
+        private void FillUserInformation<T>(List<T> list)
         {
             Console.WriteLine("***********************************************");
             Console.Write("Username: ");
-            var userName = Console.ReadLine();
-
-            if (userDictionary.ContainsKey(userName))
-                userDictionary.Remove(userName);
-            else
-                Console.WriteLine("User does not exist in the dictionary.");
-
-            return userDictionary;
-        }
-
-        private void FillUserInformation(User user)
-        {
-            Console.WriteLine("***********************************************");
-            Console.Write("Username: ");
-            user.Username = Console.ReadLine();
+            _user.Username = Console.ReadLine();
 
             Console.Write("Email: ");
-            user.Email = Console.ReadLine();
+            _user.Email = Console.ReadLine();
 
+            var location = FillLocation();
+
+            _user.City = location[0];
+            _user.PostalCode = Convert.ToInt32(location[1]);
+            _user.Country = location[2];
+
+            list.Add((T)(object)_user);
+        }
+
+        private List<string> FillLocation()
+        {
             Console.Write("City: ");
-            user.City = Console.ReadLine();
+            _city = Console.ReadLine();
 
             Console.Write("Postal Code: ");
             var postalCode = 0;
             int.TryParse(Console.ReadLine(), out postalCode);
-            user.PostalCode = postalCode;
+            _postalCode = postalCode;
 
             Console.Write("Country: ");
-            user.Country = Console.ReadLine();
+            _country = Console.ReadLine();
             Console.WriteLine("***********************************************\n");
-        }
 
-        private void ShowUsers()
-        {
-            if (userDictionary.Count == 0)
-                Console.WriteLine("There are no users in the dictionary.\n");
-            else
+            var location = new List<string>()
             {
-                Console.WriteLine("-----------------------------------------------");
-                foreach (KeyValuePair<string, User> item in userDictionary)
-                {
-                    Console.WriteLine($"Username: {item.Value.Username}");
-                    Console.WriteLine($"Email: {item.Value.Email}");
-                    Console.WriteLine($"City: {item.Value.City}");
-                    Console.WriteLine($"Postal Code: {item.Value.PostalCode}");
-                    Console.WriteLine($"Country: {item.Value.Country}");
-                    Console.WriteLine();
-                }
-                Console.WriteLine("-----------------------------------------------\n");
-            }
+                _city,
+                _postalCode.ToString(),
+                _country
+            };
+
+            return location;
         }
     }
 }
